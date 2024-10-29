a87ffce937a34d3ff77baeb96f22c25aae00c1b1
Ajay <ajaymk22@vt.edu>
2024-10-28 23:03:32 -0400

    Minor fixup

--- lldb_formatters/lldb_formatters.py
+++ lldb_formatters/lldb_formatters.py
@@ -4,4 +4,4 @@ from boost_formatter import *
 
 def __lldb_init_module(debugger, dict):
     debugger.HandleCommand('type synthetic add add -l boost_formatter.BoostSmallVectorSynthProvider -x "^boost::container::small_vector<.+>$"')
-    debugger.HandleCommand('typer summary add -F utils.SizeSummaryProvider "boost::container::small_vector<.+>$"')
+    debugger.HandleCommand('type summary add -F utils.SizeSummaryProvider -e -x "boost::container::small_vector<.+>$"')
